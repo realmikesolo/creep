8187c94405b2c1358a8784c5c809eae4c6b38979
realmikesolo <misha1810200329@gmail.com>
2021-12-30 19:36:34 +0200

    slashCommands

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,67 @@
-console.log('Tima Tverdoxleb');
+import { info } from 'console';
+import DiscordJS, { Intents } from 'discord.js';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const client = new DiscordJS.Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+});
+
+client.on('ready', () => {
+  console.log('Bot is ready');
+
+  const guildId = '925109902643458088';
+  const guild = client.guilds.cache.get(guildId);
+  let commands;
+
+  if (guild) {
+    commands = guild.commands;
+  } else {
+    commands = client.application?.commands;
+  }
+  commands?.create({
+    name: 'ping',
+    description: 'Replies with pong',
+  });
+
+  commands?.create({
+    name: 'add',
+    description: 'Adds two numbers',
+    options: [
+      {
+        name: 'num1',
+        description: 'The first number',
+        required: true,
+        type: DiscordJS.Constants.ApplicationCommandOptionTypes.NUMBER,
+      },
+      {
+        name: 'num2',
+        description: 'The second number',
+        required: true,
+        type: DiscordJS.Constants.ApplicationCommandOptionTypes.NUMBER,
+      },
+    ],
+  });
+});
+
+client.on('interactionCreate', async (interaction) => {
+  if (!interaction.isCommand()) {
+    return;
+  }
+
+  const { commandName, options } = interaction;
+
+  if (commandName === 'ping') {
+    interaction.reply({
+      content: 'pong',
+      ephemeral: true,
+    });
+  } else if (commandName === 'add') {
+    interaction.reply({
+      content: (options.getNumber('num1')! + options.getNumber('num2')!).toString(),
+      ephemeral: true,
+    });
+  }
+});
+
+client.login(process.env.TOKEN);
