d308df93adb167eda727b2052cdf26a0dea4d172
realmikesolo <misha1810200329@gmail.com>
2021-12-30 23:30:13 +0200

    firstdotatry

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { info } from 'console';
 import DiscordJS, { Intents } from 'discord.js';
 import dotenv from 'dotenv';
+import axios from 'axios';
 dotenv.config();
 
 const client = new DiscordJS.Client({
@@ -19,26 +20,39 @@ client.on('ready', () => {
   } else {
     commands = client.application?.commands;
   }
-  commands?.create({
-    name: 'ping',
-    description: 'Replies with pong',
-  });
+  // commands?.create({
+  //   name: 'ping',
+  //   description: 'Replies with pong',
+  // });
+
+  // commands?.create({
+  //   name: 'add',
+  //   description: 'Adds two numbers',
+  //   options: [
+  //     {
+  //       name: 'num1',
+  //       description: 'The first number',
+  //       required: true,
+  //       type: DiscordJS.Constants.ApplicationCommandOptionTypes.NUMBER,
+  //     },
+  //     {
+  //       name: 'num2',
+  //       description: 'The second number',
+  //       required: true,
+  //       type: DiscordJS.Constants.ApplicationCommandOptionTypes.NUMBER,
+  //     },
+  //   ],
+  // });
 
   commands?.create({
-    name: 'add',
-    description: 'Adds two numbers',
+    name: 'recentmatch',
+    description: 'Provide you statistics with your recent match',
     options: [
       {
-        name: 'num1',
-        description: 'The first number',
+        name: 'steam',
+        description: 'User steam id',
         required: true,
-        type: DiscordJS.Constants.ApplicationCommandOptionTypes.NUMBER,
-      },
-      {
-        name: 'num2',
-        description: 'The second number',
-        required: true,
-        type: DiscordJS.Constants.ApplicationCommandOptionTypes.NUMBER,
+        type: DiscordJS.Constants.ApplicationCommandOptionTypes.STRING,
       },
     ],
   });
@@ -51,17 +65,29 @@ client.on('interactionCreate', async (interaction) => {
 
   const { commandName, options } = interaction;
 
-  if (commandName === 'ping') {
+  if (commandName === 'recentmatch') {
+    const data = await axios
+      .get(`https://api.opendota.com/api/players/${options.getString('steam')}/recentMatches`)
+      .then((res) => res.data);
+
+    console.log(data);
+
     interaction.reply({
-      content: 'pong',
-      ephemeral: true,
-    });
-  } else if (commandName === 'add') {
-    interaction.reply({
-      content: (options.getNumber('num1')! + options.getNumber('num2')!).toString(),
+      content: `Id: ${data[0].match_id} \nDuration: ${data[0].duration}  `,
       ephemeral: true,
     });
   }
+  // if (commandName === 'ping') {
+  //   interaction.reply({
+  //     content: 'pong',
+  //     ephemeral: true,
+  //   });
+  // } else if (commandName === 'add') {
+  //   interaction.reply({
+  //     content: (options.getNumber('num1')! + options.getNumber('num2')!).toString(),
+  //     ephemeral: true
+  //   });
+  // }
 });
 
 client.login(process.env.TOKEN);
